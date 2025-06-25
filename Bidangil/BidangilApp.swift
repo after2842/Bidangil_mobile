ed07d10ed46bfb678388fdbb55292f8d9eed760f
Samuel <after2842@gmail.com>
2025-06-24 17:20:22 -0700

    Initial Commit

---
+++ Bidangil/BidangilApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidangilApp.swift
+//  Bidangil
+//
+//  Created by Samuel Choi on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BidangilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BidangilTests/BidangilTests.swift
@@ -0,0 +1,17 @@
+//
+//  BidangilTests.swift
+//  BidangilTests
+//
+//  Created by Samuel Choi on 6/24/25.
+//
+
+import Testing
+@testable import Bidangil
+
+struct BidangilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
